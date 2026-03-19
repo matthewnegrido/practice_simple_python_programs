2bc7879a79fc09afe26d16b03186724f594ec81c
matthewnegrido <matthewnegrido@gmail.com>
2026-03-19 19:31:46 +0800

    print the smaller number

---
+++ practice_python_programs_batch_two.py
@@ -0,0 +1,10 @@
+#Prog01
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Enter a number: "))
+
+if num_1 > num_2:
+    print(num_2)
+elif num_1 < num_2:
+    print(num_1)
+else:
+    print("Both numbers are equal!")
