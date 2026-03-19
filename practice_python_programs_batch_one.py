a9c99288299ac4dbc9cb8d30a5cece010e18c754
matthewnegrido <matthewnegrido@gmail.com>
2026-03-19 19:03:16 +0800

    two numbers, print equal if they equal

--- practice_python_programs_batch_one.py
+++ practice_python_programs_batch_one.py
@@ -8,3 +8,14 @@ elif num_1 < num_2:
     print(num_2)
 else:
     print("Both numbers are equal!")
+
+#Prog02
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Enter a number: "))
+
+if num_1 > num_2:
+    print(num_1)
+elif num_1 < num_2:
+    print(num_2)
+else:
+    print("Both numbers are equal!")
